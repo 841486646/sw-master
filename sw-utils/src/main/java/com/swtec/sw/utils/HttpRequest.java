1adb4af9211d6663465b64336574558525e74dc1
841486646 <841486646@qq.com>
2016-06-03 14:24:07 +0800

    发送短信字符指定编码格式

--- sw-utils/src/main/java/com/swtec/sw/utils/HttpRequest.java
+++ sw-utils/src/main/java/com/swtec/sw/utils/HttpRequest.java
@@ -3,7 +3,7 @@ package com.swtec.sw.utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
@@ -99,7 +99,7 @@ public class HttpRequest {
      * @return 所代表远程资源的响应结果
      */
     public static JSONObject sendPost(String url, String param) {
-        PrintWriter out = null;
+    	OutputStream out = null;
         BufferedReader in = null;
         String result= "" ;
         JSONObject array =new JSONObject();
@@ -116,9 +116,9 @@ public class HttpRequest {
             conn.setDoOutput(true);
             conn.setDoInput(true);
             // 获取URLConnection对象对应的输出流
-            out = new PrintWriter(conn.getOutputStream());
+            out = conn.getOutputStream();
             // 发送请求参数
-            out.print(param);
+            out.write(param.getBytes("UTF-8"));
             // flush输出流的缓冲
             out.flush();
             // 定义BufferedReader输入流来读取URL的响应
